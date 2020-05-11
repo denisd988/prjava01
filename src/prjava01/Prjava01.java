09b1ee33a06a8f5e9971b79fd79f3a4acadd20e4
denis <dedy756@vidalibarraquer.net>
2020-05-11 02:32:01 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -16,9 +16,10 @@ public static void main(String[] args) throws IOException { File f = new File("f
 try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
 bw.write("<html>");bw.newLine(); bw.write("  <head>");bw.newLine(); bw.write("    <title>");bw.newLine();
 bw.write("      Nova p&agrave;gina  web");bw.newLine();
-bw.write("    </title>");bw.newLine(); bw.write("  </head>");bw.newLine(); bw.write("  <body>");bw.newLine();
-bw.write("    Nova p&agrave;gina web");bw.newLine(); bw.write("  </body>");bw.newLine(); bw.write("</html>");bw.newLine();
+bw.write("    </title>");bw.newLine(); bw.write("  </head>");bw.newLine(); bw.write("  <body>");bw.newLine(); 
 bw.write("    <h1>DAM2 m05uf2pr4</h1>");bw.newLine();
+bw.write("    Nova p&agrave;gina web");bw.newLine(); bw.write("  </body>");bw.newLine(); bw.write("</html>");bw.newLine();
+
 bw.close();
 }
 }
